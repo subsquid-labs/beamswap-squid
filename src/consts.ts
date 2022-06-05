8f09365f5558d186420b7de335f5c65cb28f0095
Alexander Belopashentsev <61732514+belopash@users.noreply.github.com>
2022-06-05 23:32:36 +0500

    change endpoint and factory

--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,12 @@
 export const knownContracts: ReadonlyArray<string> = [];
 
-export const CHAIN_NODE = "wss://moonbase-alpha.public.blastapi.io";
+export const CHAIN_NODE = "wss://moonbeam.public.blastapi.io";
 
-export const FACTORY_ADDRESS = "0x9ea55cc5b43d651d7d0e99e3a01c80854853f54d";
+export const FACTORY_ADDRESS = "0x985bca32293a7a496300a48081947321177a86fd";
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 
 export const ZERO_BI = 0n;
 export const ONE_BI = 1n;
 export const ZERO_BD = 0;
 export const ONE_BD = 1;
-export const BI_18 = 18n;
+export const BI_18 = 1000000000000000000n;
